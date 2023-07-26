1c0c7b876bf3d6421fdd3f237fe022e98262d49f
avhan45 <avhan45@gmail.com>
2023-07-26 08:21:21 -0400

    export db

--- perumda_5.sql
+++ perumda_5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 23 Jul 2023 pada 14.25
+-- Waktu pembuatan: 26 Jul 2023 pada 14.20
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -37,8 +37,7 @@ CREATE TABLE `Blok` (
 --
 
 INSERT INTO `Blok` (`no_blok`, `nama_blok`) VALUES
-('01', 'A'),
-('02', 'A');
+('01', 'A');
 
 -- --------------------------------------------------------
 
@@ -56,8 +55,9 @@ CREATE TABLE `Klasifikasi` (
 --
 
 INSERT INTO `Klasifikasi` (`id_klasifikasi`, `klasifikasi`) VALUES
-(1, 'Swadaya'),
-(2, 'Links');
+(1, 'LODS SWADAYA'),
+(2, 'KIOS REVITALISASI'),
+(4, 'LODS INPRES');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,8 @@ CREATE TABLE `Pasar` (
 INSERT INTO `Pasar` (`no_pasar`, `nama_pasar`, `alamat_pasar`) VALUES
 ('05', 'Baruga', 'Baruga'),
 ('06', 'Lapulu', 'Lapulu'),
-('07', 'Andonohu', 'Andonohu');
+('07', 'Andonohu', 'Andonohu'),
+('08', 'Mandonga', 'Mandonga');
 
 -- --------------------------------------------------------
 
@@ -92,13 +93,14 @@ CREATE TABLE `Pedagang` (
   `no_blok` varchar(10) DEFAULT NULL,
   `id_klasifikasi` int(11) DEFAULT NULL,
   `nama_pedagang` varchar(100) DEFAULT NULL,
-  `jk` varchar(1) DEFAULT NULL,
+  `ktp` varchar(200) NOT NULL,
+  `jk` varchar(20) DEFAULT NULL,
   `agama` varchar(20) DEFAULT NULL,
   `no_hp` varchar(20) DEFAULT NULL,
   `ukuran` varchar(20) DEFAULT NULL,
   `alamat` varchar(200) DEFAULT NULL,
   `jenis_usaha` varchar(100) DEFAULT NULL,
-  `sertifikat` varchar(50) DEFAULT NULL,
+  `sertifikat` varchar(120) NOT NULL,
   `keterangan` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -106,11 +108,30 @@ CREATE TABLE `Pedagang` (
 -- Dumping data untuk tabel `Pedagang`
 --
 
-INSERT INTO `Pedagang` (`id_pedagang`, `no_pasar`, `no_blok`, `id_klasifikasi`, `nama_pedagang`, `jk`, `agama`, `no_hp`, `ukuran`, `alamat`, `jenis_usaha`, `sertifikat`, `keterangan`) VALUES
-(1, '05', '01', 1, 'Aco', 'L', 'islam', '123456', '3', 'Baruga', 'Sembako', 'Ada', 'Aktif'),
-(2, '05', '02', 2, 'Putri', 'P', 'Islam', '567890', '2', 'Andonohu', 'Sayuran', 'Ada', 'Belum Aktif'),
-(3, '06', '01', 1, 'sadasd', 'L', 'asd', '2131', '213', 'adasd', 'asdasd', 'asd', 'asdad'),
-(4, '06', '02', 2, 'dsdsds', 'a', 'asd', '123', '3123', 'asdasda', 'asdada', 'asdasd', 'asdasd');
+INSERT INTO `Pedagang` (`id_pedagang`, `no_pasar`, `no_blok`, `id_klasifikasi`, `nama_pedagang`, `ktp`, `jk`, `agama`, `no_hp`, `ukuran`, `alamat`, `jenis_usaha`, `sertifikat`, `keterangan`) VALUES
+(26, '05', '01', 1, 'asd', '1690373092_be06eb92de9959b98321.jpg', 'Laki-laki', 'asd', '12312', '123', 'asd', 'adasd', '', 'asd'),
+(36, '07', '01', 1, 'Agus', '1690373817_13751f9fc92c58b3bade.jpg', 'Laki-laki', 'Islam', '123123123', 'asdad', 'asd', 'asdasdad', '', 'asd');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `Sertifikat`
+--
+
+CREATE TABLE `Sertifikat` (
+  `id_sertifikat` int(11) NOT NULL,
+  `id_pedagang` int(11) NOT NULL,
+  `no_sertifikat` varchar(100) NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `Sertifikat`
+--
+
+INSERT INTO `Sertifikat` (`id_sertifikat`, `id_pedagang`, `no_sertifikat`, `image`) VALUES
+(4, 26, '123123', '1690373161_5dfb538ad8fa6c984dd2.jpeg'),
+(14, 36, '213asd', '1690373674_bd74f31306b86cf38f4c.jpg');
 
 -- --------------------------------------------------------
 
@@ -160,9 +181,17 @@ ALTER TABLE `Pasar`
 --
 ALTER TABLE `Pedagang`
   ADD PRIMARY KEY (`id_pedagang`),
-  ADD KEY `no_pasar` (`no_pasar`),
-  ADD KEY `no_blok` (`no_blok`),
-  ADD KEY `id_klasifikasi` (`id_klasifikasi`);
+  ADD KEY `id_klasifikasi` (`id_klasifikasi`),
+  ADD KEY `Pedagang_ibfk_1` (`no_pasar`),
+  ADD KEY `Pedagang_ibfk_2` (`no_blok`);
+
+--
+-- Indeks untuk tabel `Sertifikat`
+--
+ALTER TABLE `Sertifikat`
+  ADD PRIMARY KEY (`id_sertifikat`),
+  ADD KEY `id_pedagang` (`id_pedagang`),
+  ADD KEY `id_pedagang_2` (`id_pedagang`);
 
 --
 -- Indeks untuk tabel `Users`
@@ -178,13 +207,19 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT untuk tabel `Klasifikasi`
 --
 ALTER TABLE `Klasifikasi`
-  MODIFY `id_klasifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_klasifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `Pedagang`
 --
 ALTER TABLE `Pedagang`
-  MODIFY `id_pedagang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pedagang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT untuk tabel `Sertifikat`
+--
+ALTER TABLE `Sertifikat`
+  MODIFY `id_sertifikat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `Users`
@@ -200,9 +235,15 @@ ALTER TABLE `Users`
 -- Ketidakleluasaan untuk tabel `Pedagang`
 --
 ALTER TABLE `Pedagang`
-  ADD CONSTRAINT `Pedagang_ibfk_1` FOREIGN KEY (`no_pasar`) REFERENCES `Pasar` (`no_pasar`),
-  ADD CONSTRAINT `Pedagang_ibfk_2` FOREIGN KEY (`no_blok`) REFERENCES `Blok` (`no_blok`),
-  ADD CONSTRAINT `Pedagang_ibfk_3` FOREIGN KEY (`id_klasifikasi`) REFERENCES `Klasifikasi` (`id_klasifikasi`);
+  ADD CONSTRAINT `Pedagang_ibfk_1` FOREIGN KEY (`no_pasar`) REFERENCES `Pasar` (`no_pasar`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Pedagang_ibfk_2` FOREIGN KEY (`no_blok`) REFERENCES `Blok` (`no_blok`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Pedagang_ibfk_3` FOREIGN KEY (`id_klasifikasi`) REFERENCES `Klasifikasi` (`id_klasifikasi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `Sertifikat`
+--
+ALTER TABLE `Sertifikat`
+  ADD CONSTRAINT `sertifikat_ibfk` FOREIGN KEY (`id_pedagang`) REFERENCES `Pedagang` (`id_pedagang`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
